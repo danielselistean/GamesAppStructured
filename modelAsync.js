39d91165195a64346ad160171afc694a7e0084df
Daniel <daniel.selistean@yahoo.com>
2020-04-25 07:55:54 +0300

    adding try-catch

--- modelAsync.js
+++ modelAsync.js
@@ -2,60 +2,80 @@
 var apiURL = "https://games-app-siit.herokuapp.com";
 
   async function getGamesList(){
-    const response = await  fetch(apiURL + "/games", {
+    try{
+        const response = await  fetch(apiURL + "/games", {
                 method: "GET",
                 headers: {
                     "Content-Type": "application/x-www-form-urlencoded"
                 }
-            });
-            console.log(response);
-    const arrayOfGames = await response.json();
-        return arrayOfGames;
+        });
+                console.log(response);
+
+        const arrayOfGames = await response.json();
+            return arrayOfGames;
+
+    } catch(errorMsg){
+        console.log("An error has occured: ", errorMsg);
+    }
 
 }
 
 
 async function deleteGame(gameID){
-  const response = await  fetch(apiURL + "/games/" + gameID, {
-               method: "DELETE",
-               headers: {
-                "Content-Type": "application/x-www-form-urlencoded"
-            }
+    try{
+        const response = await  fetch(apiURL + "/games/" + gameID, {
+                    method: "DELETE",
+                     headers: {
+                     "Content-Type": "application/x-www-form-urlencoded"
+                     }
         });
-        console.log(response);
+                console.log(response);
 
-  const apiresponse = await response.text();
-  console.log(apiresponse);
-    return apiresponse;
+        const apiresponse = await response.text();
+        console.log(apiresponse);
+            return apiresponse;
+
+    }catch(errorMsg){
+        console.log("An error has occured: ", errorMsg);
+    }
 }
 
 
 async function createGameRequest(gameObject){
+    try{
+        const response = await fetch(apiURL + "/games", {
+                    method: "POST",
+                    headers: {
+                         "Content-Type": "application/x-www-form-urlencoded"
+                     },
+                    body: gameObject
+        });
 
-  const response = await fetch(apiURL + "/games", {
-              method: "POST",
-              headers: {
-                  "Content-Type": "application/x-www-form-urlencoded"
-              },
-              body: gameObject
-          });
+        const createdGame = await response.json();
+        console.log(createdGame);
+            return createdGame;
 
-  const createdGame = await response.json();
-  console.log(createdGame);
-    return createdGame;
+    }catch(errorMsg){
+        console.log("An error has occured: ", errorMsg);
+    }
 }
 
 
 async function updateGameRequest(gameID, updatedGameObject){
-    const response = await fetch(apiURL + "/games/" + gameID, {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
-        body: updatedGameObject
-});
+    try{
+        const response = await fetch(apiURL + "/games/" + gameID, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            body: updatedGameObject
+        });
 
-    const updatedGame = await response.json();
-    console.log(updatedGame);
-        return updatedGame;
+        const updatedGame = await response.json();
+        console.log(updatedGame);
+            return updatedGame;
+
+    }catch(errorMsg){
+        console.log("An error has occured: ", errorMsg);
+    }    
 }
